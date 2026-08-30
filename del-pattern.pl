257487a7499e1cd30a87e347734248ebfe31842e
4g4 <lispainter@163.com>
2026-08-30 18:19:53 +0800

    delete pattern in filenames

---
+++ del-pattern.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/env perl
+
+use utf8;
+use Encode;
+use Path::Tiny;
+use Getopt::Long;
+
+my $dir_path = ".";
+my $ext_name = 'pdf';
+my $pattern = '';
+my $dry_run = '';
+my $help = 0;
+
+GetOptions (
+  'dir=s' => \$dir_path,
+  'ext=s' => \$ext_name,
+  'pattern=s' => \$pattern,
+  'dry' => \$dry_run,
+  'help' => \$help,
+) or die("Error in command line arguments\n");
+
+if ($help) {
+      print <<"HELP";
+用法: $0 [选项]
+
+选项:
+  --dir=DIR       目标目录（默认: .）
+  --ext=EXT       文件扩展名（默认: pdf，不带点）
+  --pattern=PAT   要删除的字符串或正则表达式
+  --dry           预览模式，不实际执行
+  --help          显示此帮助
+
+示例:
+  $0 --pattern="abc" --dry
+  $0 --pattern="def" --ext=mp4
+HELP
+  exit;
+}
+
+if (length $pattern == 0) {
+  print "--pattern is required\n";
+  exit;
+}
+
+my $dir = path($dir_path);
+my $iter = $dir->iterator;
+
+while (my $file = $iter->()) {
+  next if $file->is_dir();
+  my $basename = $file->basename;
+  next unless $basename =~ /\.$ext_name$/i;
+
+  my $parent = $file->parent;
+  my $new_base = $basename;
+  $new_base =~ s/$pattern//g;
+
+  if ($basename ne $new_base) {
+    my $new_file = $parent->child($new_base);
+    if ($new_file->exists) {
+      print("warning: $new_file exists, skip $basename\n");
+      next;
+    }
+
+    if ($dry_run) {
+      print("$basename\t=>\t$new_base\n");
+    } else {
+      print("$file\t=>\t$new_file\n");
+      $file->move($new_file);
+    }
+  }
+}
