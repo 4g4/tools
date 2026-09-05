01b0623ce279438632ca152079b408b1e021870a
liuxueyang <liuxueyang457@gmail.com>
2026-09-05 23:25:59 +0800

    Update print format

--- del-pattern.pl
+++ del-pattern.pl
@@ -62,9 +62,9 @@ while (my $file = $iter->()) {
     }
 
     if ($dry_run) {
-      print("$basename\t=>\t$new_base\n");
+      print("$basename\n=>\t$new_base\n");
     } else {
-      print("$file\t=>\t$new_file\n");
+      print("$file\n=>\t$new_file\n");
       $file->move($new_file);
     }
   }
