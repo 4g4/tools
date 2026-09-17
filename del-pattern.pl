d30d288a349ae51f33af8ac9f83de8e5ade6d889
liuxueyang <liuxueyang457@gmail.com>
2026-09-17 11:52:33 +0800

    Add Z-Library pattern

--- del-pattern.pl
+++ del-pattern.pl
@@ -9,6 +9,7 @@ my $dir_path = ".";
 my $ext_name = 'pdf';
 my $pattern = '';
 my $dry_run = '';
+my $zlib = '';
 my $help = 0;
 
 GetOptions (
@@ -16,6 +17,7 @@ GetOptions (
   'ext=s' => \$ext_name,
   'pattern=s' => \$pattern,
   'dry' => \$dry_run,
+  'zlib' => \$zlib,
   'help' => \$help,
 ) or die("Error in command line arguments\n");
 
@@ -28,6 +30,7 @@ if ($help) {
   --ext=EXT       文件扩展名（默认: pdf，不带点）
   --pattern=PAT   要删除的字符串或正则表达式
   --dry           预览模式，不实际执行
+  --zlib          Z-Library Pattern
   --help          显示此帮助
 
 示例:
@@ -35,6 +38,10 @@ if ($help) {
   $0 --pattern="def" --ext=mp4
 HELP
   exit;
+}
+
+if ($zlib) {
+  $pattern = " \(z-library.sk, 1lib.sk, z-lib.sk\)";
 }
 
 if (length $pattern == 0) {
